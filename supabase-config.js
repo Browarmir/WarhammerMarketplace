26fbcba8beee9e73eb6ae18a8c62e2ef0146be4b
Browarmir <boguslawski02wp.pl@gmail.com>
2024-08-18 21:33:15 +0200

    #update

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,9 @@
-const { createClient } = supabase;
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://cgqwklizyykmrurgbeoh.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNncXdrbGl6eXlrbXJ1cmdiZW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwMDIwMTUsImV4cCI6MjAzOTU3ODAxNX0.L0vvjYinKNGkPNgh-PqHYMuHzTjuDC-_URFFajt7aOU';
+// Your Supabase project URL and public ANON_KEY
+const supabaseUrl = 'https://cgqwklizyykmrurgbeoh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNncXdrbGl6eXlrbXJ1cmdiZW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwMDIwMTUsImV4cCI6MjAzOTU3ODAxNX0.L0vvjYinKNGkPNgh-PqHYMuHzTjuDC-_URFFajt7aOU';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
